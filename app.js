6aee44c7eab93c25b1527db92e4c7ef3706ec4c5
palkim <d7deniz@windowslive.com>
2020-08-19 21:02:33 +0300

    first working

--- app.js
+++ app.js
@@ -1,28 +1,65 @@
 //@ts-check
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
+const express = require('express');
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true";
+//const cors = require('cors');
+const app = express();
+app.use(express.json());
+const url = "mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true";
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  console.log(db.listDatabases());
-  db.close();
-});
+// TODO refactor to promise
+let getTotalCount = function(min_date, max_date, min_count, max_count, callback){ // callback(err, result)
+    MongoClient.connect(url, (err, db) => {
+        if (err) throw err;
+        var dbo = db.db("getir-case-study");
+        var query = {  };
+        dbo.collection("records").aggregate([
+            {
+               "$match": {
+                   "createdAt": { "$lt": new Date(max_date+"T00:00:00.000Z"), "$gt": new Date(min_date + "T00:00:00.000Z"), } // TODO check lte gte // TODO better date parsing
+               }
+            },
+            {
+                "$project": {
+                    "_id" : 0 , //carry this to after thye last match if neccesey
+                    "totalCount" : { "$sum" : "$counts"},
+                    "createdAt" : 1,
+                    "key" : 1
+                   
+               }
+            },
+            {
+                "$match": {
+                    "totalCount": { "$lt": max_count, "$gt":min_count} // TODO check lte gte
+                }
+            }
+       ]).toArray(callback
+        );
+        });
+}
+
+
+
+app.post('/enpoint/', function (req, res) {
+    let response_json = req.body
+
+    
+    getTotalCount(response_json.startDate, response_json.endDate, response_json.minCount, response_json.maxCount, (err,data) => {
+        res.setHeader('Content-Type', 'application/json');
+        console.log(err); // TODO handle error
+        console.log(data);
+        res.end(JSON.stringify(data));
+    })
+
+   
+})
+
+app.listen(3000, () => {
+    console.log(`Example app listening at http://localhost:3000`)
+    //console.log(`Example app listening at http://localhost:${port}`)
+  })
+
+
+
 
 
 
